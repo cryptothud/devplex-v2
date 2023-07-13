1fa5c3b84834b19eca0dd8b47346264c0365b8e2
Thud <thuddesigns@gmail.com>
2023-07-13 06:10:20 -0700

    add theme color metadata

--- src/components/navbar/index.tsx
+++ src/components/navbar/index.tsx
@@ -5,6 +5,7 @@ import { BsTwitter, BsDiscord } from "react-icons/bs"
 import { HiMoon } from "react-icons/hi"
 import { BiSun, BiMoon } from "react-icons/bi"
 import { useMediaQuery } from 'react-responsive'
+import Head from 'next/head'
 
 export const Navbar = () => {
 
@@ -39,22 +40,27 @@ export const Navbar = () => {
     }
 
     return (
-        <div className="fixed w-screen top-0 left-0 py-4 px-4 sm:px-8 z-[2] flex justify-between items-center">
-            <div className="flex justify-center items-center gap-2 select-none pointer-events-none">
-                <Image src={darkMode ? "/images/devplex-light.svg" : "/images/devplex-color.svg"} alt="Logo" width={70} height={70} className="w-[50px] sm:w-[70px] h-[50px] sm:h-[70px]" />
-                <h1 className="main-text-color text-3xl sm:text-4xl font-medium">Devplex</h1>
+        <>
+            <Head>
+                <meta name="theme-color" content={darkMode ? "#1D1F20" : "#fafafa"} />
+            </Head>
+            <div className="fixed w-screen top-0 left-0 py-4 px-4 sm:px-8 z-[2] flex justify-between items-center">
+                <div className="flex justify-center items-center gap-2 select-none pointer-events-none">
+                    <Image src={darkMode ? "/images/devplex-light.svg" : "/images/devplex-color.svg"} alt="Logo" width={70} height={70} className="w-[50px] sm:w-[70px] h-[50px] sm:h-[70px]" />
+                    <h1 className="main-text-color text-3xl sm:text-4xl font-medium">Devplex</h1>
+                </div>
+                <div className="flex justify-center gap-2 items-center">
+                    <a className="flex justify-center items-center hover:opacity-90 cursor-pointer" href="https://twitter.com/CryptoThud" target="_blank">
+                        <BsTwitter size={22} color={darkMode ? "#fafafa" : "#1D1F20"} />
+                    </a>
+                    <a className="flex justify-center items-center hover:opacity-90 cursor-pointer" href="https://discord.com/users/654481823585271852" target="_blank">
+                        <BsDiscord size={22} color={darkMode ? "#fafafa" : "#1D1F20"} />
+                    </a>
+                    <a className="flex justify-center items-center hover:opacity-90 cursor-pointer" onClick={handleDarkMode}>
+                        {darkMode ? <BiSun size={22} color={darkMode ? "#fafafa" : "#1D1F20"} /> : <HiMoon size={22} color={darkMode ? "#fafafa" : "#1D1F20"} />}
+                    </a>
+                </div>
             </div>
-            <div className="flex justify-center gap-2 items-center">
-                <a className="flex justify-center items-center hover:opacity-90 cursor-pointer" href="https://twitter.com/CryptoThud" target="_blank">
-                    <BsTwitter size={22} color={darkMode ? "#fafafa" : "#1D1F20"} />
-                </a>
-                <a className="flex justify-center items-center hover:opacity-90 cursor-pointer" href="https://discord.com/users/654481823585271852" target="_blank">
-                    <BsDiscord size={22} color={darkMode ? "#fafafa" : "#1D1F20"} />
-                </a>
-                <a className="flex justify-center items-center hover:opacity-90 cursor-pointer" onClick={handleDarkMode}>
-                    {darkMode ? <BiSun size={22} color={darkMode ? "#fafafa" : "#1D1F20"} /> : <HiMoon size={22} color={darkMode ? "#fafafa" : "#1D1F20"} />}
-                </a>
-            </div>
-        </div>
+        </>
     )
 }
