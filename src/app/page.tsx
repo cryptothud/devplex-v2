55e8c18cdefcf1a1d9a3fd9f82ebe9a9cf2ed036
Thud <thuddesigns@gmail.com>
2023-10-17 02:20:13 -0700

    fix height again

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -17,7 +17,7 @@ export default function Home() {
 
   return (
     <main className="flex min-h-screen flex-col items-center justify-center px-4 sm:px-24 relative z-[1]">
-      <div className="flex flex-col gap-4 items-center justify-center h-screen relative">
+      <div className="flex flex-col gap-4 items-center justify-center h-screen min-h-screen relative">
         <h1 className="text-4xl sm:text-6xl main-text-color font-medium text-center"><span className="text-[#E1AE2A]">Your</span> Vision. <span className="text-[#658EBE]">Our</span> Code.</h1>
         <h2 className="text-base main-text-color opacity-50 text-center tracking-wide">We help bring your vision to life and onto the blockchain.</h2>
         <a href="mailto:contact@devplex.org">
